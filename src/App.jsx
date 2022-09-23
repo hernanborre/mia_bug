581fedf168043d8a75432bd971140cb05e06bf9f
Hernan Borre <hborre@vi-datec.com>
2022-09-23 18:04:54 -0300

    add route redirect y anchor

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,36 @@ import "./App.css";
 import {Link, useHref} from "react-router-dom";
 
 function App() {
-  let href = useHref("https://www.google.com.ar");
+  //let href = useHref("https://www.google.com.ar");
   return (
     <div>
       <h1>Home</h1>
       <nav>
         <Link to="/">Home</Link> | <Link to="about">About</Link> |{" "}
-        <Link
-          to={href}
-          target="_blank"
-          reloadDocument
-          rel="noopener noreferrer"
-        >
-          Google
+        <Link to="/redirect">
+          Yahoo router redirect
         </Link>
+        {" "}|{" "}
+        <Link
+          to={{pathname: "https://google.com/" }}
+          target="_blank"
+        >
+          Google Link
+        </Link>
+        {" "}|{" "}
+        <a href="http://google.com" target='_blank'>Google anchor</a>
       </nav>
     </div>
   );
 }
+
+//https://github.com/remix-run/react-router/discussions/8611 
+//solo se debería usar Link cuando queres que sea linkeado adentro de tu app
+
+const Redirect = ( props ) => {
+  const { url } = props
+  return <h5>Redirecting...</h5>;
+};
+
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Redirect from "./Redirect";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}></Route>
+        <Route path="/redirect" element={<Redirect url="http://yahoo.com"/>} ></Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
